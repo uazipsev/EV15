a6d9c0159b3b13b9a9cb25c02bd180d141953524
uazipsev <uazipsev@gmail.com>
2015-03-25 19:50:32 -0400

    Fixed compile issue

--- BMS-HV.X/Global.c
+++ BMS-HV.X/Global.c
@@ -10,8 +10,8 @@
 #ifndef GLOBAL_C
 #define	GLOBAL_C
 
-char Temp_Get = 0;
-char Volt_Get = 0;
+char Temp_Aquire = 0;
+char Volt_Aquire = 0;
 int Battery_Adc[10] = 0;
 int Temp_Adc[10] = 0;
 int ADC_Buffer_Point = 0;
