801ca1fb4f5b3d259a7f683bbe851b021ed59844
richcj10 <richcj10@gmail.com>
2015-05-27 19:43:22 -0400

    added commenting

--- PDU-BP.X/Shift595.c
+++ PDU-BP.X/Shift595.c
@@ -3,29 +3,39 @@
 #include "xc.h"
 #include "Functions.h"
 
-
+/*
+ * On init we want to clear out the shift Reg.
+ * This code does that.
+ */
 void StartUp595()
 {
     //Shut off outputs to get ready for config!
     OUTEN_SetHigh();
     //clear all shift reg's
-    Delay(10);
+    Delay(1);
     CLEAR_SetHigh();
-    Delay(10);
+    Delay(1);
     CLEAR_SetLow();
-    Delay(10);
+    Delay(1);
     CLEAR_SetHigh();
-    Delay(10);
+    Delay(1);
     //Init Config Done! Set for regular updates
     OUTEN_SetLow();
     Clear595();
 }
 
+/*
+ * This piece of code allows use to set bits in the right paces to set outputs of the shift reg's
+ * The fcn can't augment the array so you need to make sure the array is big enugh for the application
+ */
 void SetPin595(int Reg, int pin, int value)
 {
    registers[((Reg-1)*8)+pin] = value;
 }
 
+/*
+ *  This will clear the aray and set all outputs to zero
+ */
 void Clear595()
 {
   for(int i = RegPins - 1; i >=  0; i--){
@@ -34,12 +44,15 @@ void Clear595()
   writeRegisters();
 }
 
+/*
+ *  This is a fcn that will set outputs based the private array of this c file
+ */
 void writeRegisters(){
 
-  LAT_SetLow();
+  LAT_SetLow();  // set select low
 
   for(int i = RegPins - 1; i >=  0; i--){
-    CLK_SetLow();
+    CLK_SetLow();   //Set clock low (data is shifted in on rising edge)
 
     int val = registers[i];
     if (val == HIGH )
@@ -50,10 +63,10 @@ void writeRegisters(){
     {
         DATA_OUT_SetLow();
     }
-    CLK_SetHigh();
+    CLK_SetHigh();  //shift data in
 
   }
-  LAT_SetHigh();
+  LAT_SetHigh();  //Set latch high puting sent data to output
 
 }
 
