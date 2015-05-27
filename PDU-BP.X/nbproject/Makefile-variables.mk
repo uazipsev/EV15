4a91e725e4e5aef93530ff2ed22959bb127e6602
richcj10 <richcj10@gmail.com>
2015-05-27 19:24:51 -0400

    Fixed PDU project corruption

--- PDU-BP.X/nbproject/Makefile-variables.mk
+++ PDU-BP.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=PDU-BP.X.production.null
-CND_ARTIFACT_PATH_default=dist/default/production/PDU-BP.X.production.null
+CND_ARTIFACT_NAME_default=PDU-BP.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PDU-BP.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=pdu-bp.x.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pdu-bp.x.tar
