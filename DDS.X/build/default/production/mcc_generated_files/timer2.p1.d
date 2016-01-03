9e327e71cc589608d4ff39739e92322e8c53cd2f
drummerboy2543 <att11@zips.uakron.edu>
2016-01-02 21:47:43 -0500

    Re did some code so some of the files stay consistent. The other commit I think had a lot of other stuff that was not necessary Official Update for blink LED.
    -Andrew Thornborough

--- DDS.X/build/default/production/mcc_generated_files/timer2.p1.d
+++ DDS.X/build/default/production/mcc_generated_files/timer2.p1.d
@@ -1,6 +1,6 @@
- build/default/production/IO.d  \
- build/default/production/IO.p1:  \
- IO.c  \
+ build/default/production/mcc_generated_files/timer2.d  \
+ build/default/production/mcc_generated_files/timer2.p1:  \
+ mcc_generated_files/timer2.c  \
 mcc_generated_files/mcc.h  \
 mcc_generated_files/../UART.h  \
 mcc_generated_files/adc.h  \
